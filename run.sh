69ada8d056a32657c46c8a0fcc32f2d8baff3b4d
Pyry-Peetri Piironen <pyry13@Pyry-Peetri-MacBook-Pro.local>
2022-12-14 17:21:59 +0200

    More maps to test.

--- run.sh
+++ run.sh
@@ -2,74 +2,38 @@ make re
 make clean
 clear
 
+echo ""
+echo "START HERE"
+echo "START HERE"
+echo "START HERE"
+echo ""
+
+# # * * * * * * * * * * TEST ERRORS * * * * * * * * * *
+# echo "Test all error maps from ./maps/error_maps/"
+# echo ""
 # for f in ./maps/error_maps/*
 # do
-# 	leaks -atExit -- ./lem-in < $f
+# 	./lem-in < $f
+# done
+
+# # * * * * * * * * * * TEST AUGMENTS * * * * * * * * * *
+# echo "Test all augment maps from ./maps/cases/"
+# echo ""
+# for f in ./maps/cases/*
+# do
+# 	echo ""
+# 	echo "NEXT ONE: $f"
+# 	./lem-in -t < $f
+# done
+
+# # * * * * * * * * * * TEST BASICS * * * * * * * * * * // varmista että toimii
+# echo "Test all basic maps from ./maps/basic/"
+# echo ""
+# for f in ./maps/basic/*
+# do
+# 	echo ""
+# 	echo "NEXT ONE: $f"
+# 	./lem-in -t < $f
 # done
 
 
-time ./lem-in < maps/generator_maps/gen_big_0
-time ./lem-in -m < maps/generator_maps/gen_big_1
-time ./lem-in -m < maps/generator_maps/gen_big_2
-time ./lem-in -m < maps/generator_maps/gen_big_3
-
-
-# echo "BIG SUP"
-# echo "START HERE"
-# echo "BIG SUP"
-# echo "START HERE"
-# echo "BIG SUP"
-# echo "START HERE"
-# echo "BIG SUP"
-# echo "START HERE"
-
-
-time ./lem-in  -m < maps/generator_maps/map_big_sup_1
-time ./lem-in -m < maps/generator_maps/map_big_sup_2
- time ./lem-in -m < maps/generator_maps/map_big_sup_3
- time ./lem-in -m < maps/generator_maps/map_big_sup_4
- time ./lem-in -m < maps/generator_maps/map_big_sup_5
-time ./lem-in -m < maps/generator_maps/map_big_sup_6
- time ./lem-in -m < maps/generator_maps/map_big_sup_7
-time ./lem-in -m < maps/generator_maps/map_big_sup_8
- time ./lem-in -m < maps/generator_maps/map_big_sup_9
-time ./lem-in -m < maps/generator_maps/map_big_sup_10
-time ./lem-in -m < maps/generator_maps/map_big_sup_11
- time ./lem-in -m < maps/generator_maps/map_big_sup_12
-time ./lem-in -m < maps/generator_maps/map_big_sup_13
-time ./lem-in -m < maps/generator_maps/map_big_sup_14
-time ./lem-in -m < maps/generator_maps/map_big_sup_15
- time ./lem-in -m < maps/generator_maps/map_big_sup_16
- time ./lem-in -m < maps/generator_maps/map_big_sup_17
-time ./lem-in -m < maps/generator_maps/map_big_sup_18
-time ./lem-in -m < maps/generator_maps/map_big_sup_19
-time ./lem-in -m < maps/generator_maps/map_big_sup_20
-time ./lem-in -m < maps/generator_maps/map_big_sup_21
-time ./lem-in -m < maps/generator_maps/map_big_sup_22
-time ./lem-in -m < maps/generator_maps/map_big_sup_23
-time ./lem-in -m < maps/generator_maps/map_big_sup_24
-time ./lem-in -m < maps/generator_maps/map_big_sup_25
-time ./lem-in -m < maps/generator_maps/map_big_sup_26
- time ./lem-in -m < maps/generator_maps/map_big_sup_27
-time ./lem-in -m < maps/generator_maps/map_big_sup_29
-time ./lem-in -m < maps/generator_maps/map_big_sup_30
-
-
-# time ./lem-in < mini_gauntlet/cases/augment_0.map
-#  time ./lem-in -m < mini_gauntlet/cases/augment_5.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_2.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_3.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_4.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_5.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_6.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_7.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_8.map
-# time ./lem-in -m < mini_gauntlet/cases/augment_9.map
-
-
-
-# echo "START HERE"
-# echo "START HERE"
-# echo "START HERE"
-
-# ./lem-in < maps/augment_maps/augment_0.map
